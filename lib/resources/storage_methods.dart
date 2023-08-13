3fb94623a11e11e9b2eea6741268f4f89d932664
vishnuprasad01 <vishnuprasad.mukundan@gmail.com>
2023-08-13 23:09:54 +0530

    storage methods created for future posts and profile pic

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -1,9 +1,28 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 
 class StorageMethods {
   FirebaseStorage _storage = FirebaseStorage.instance;
+  FirebaseAuth _auth = FirebaseAuth.instance;
+ 
   //add image to storage
+  Future<String> uploadImageToStorage(
+      String childName, Uint8List file, bool isPost) async {
+    // creating location to our firebase storage
 
+    Reference ref =
+        _storage.ref().child(childName).child(_auth.currentUser!.uid);
+    if (isPost) {
+      String id = id;
+      ref = ref.child(id);
+    }
+
+    // putting in uint8list format -> Upload task like a future but not future
+    UploadTask uploadTask = ref.putData(file);
+
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
 }
-
