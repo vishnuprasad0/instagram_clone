35e8d657bf474a2c0ead26c76d6576c5541f27c7
vishnuprasad01 <vishnuprasad.mukundan@gmail.com>
2023-08-16 22:43:10 +0530

    created bottom bar according to instagram correctly

--- lib/responsive/mobilescreen_layout.dart
+++ lib/responsive/mobilescreen_layout.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/models/user_model.dart' as model;
-import 'package:instagram_clone/providers/user_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:instagram_bottom_nav_bar/instagram_tab_view.dart';
+import 'package:instagram_clone/constants/colors.dart';
+import 'package:instagram_clone/constants/instagram_icons_icons.dart';
+
+// import 'package:instagram_clone/models/user_model.dart' as model;
+// import 'package:instagram_clone/providers/user_provider.dart';
+// import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -13,7 +17,43 @@ class MobileScreenLayout extends StatefulWidget {
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   @override
   Widget build(BuildContext context) {
-    model.User user = Provider.of<UserProvider>(context).getUser;
-    return Scaffold(body: Center(child: Text(user.bio)));
+    // model.User user = Provider.of<UserProvider>(context).getUser;
+    // return Scaffold(body: Center(child: Text(user.bio)));
+    return Scaffold(
+        body: const Center(child: Text('mobile screen')),
+        bottomNavigationBar: InstagramTabView(
+            iconSize: 20,
+            backgroundColor: Colors.black,
+            selectedItemColor: primaryColor,
+            unselectedItemColor: primaryColor,
+            dividerColor: primaryColor,
+            selectedIconTheme: const IconThemeData(
+                color: primaryColor, fill: 1.0, size: 22, grade: 0.2),
+            isDivider: true,
+            unselectedIconTheme: const IconThemeData(color: secondaryColor),
+            items: [
+              InstagramTabItem(
+                label: '',
+                page: const Text(''),
+                icon: InstagramIcons.instagram_home,
+              ),
+              InstagramTabItem(
+                  label: '',
+                  page: const Text(''),
+                  icon: InstagramIcons.instagram_search),
+              InstagramTabItem(
+                  label: '',
+                  page: const Text(''),
+                  icon: InstagramIcons.addpost),
+              InstagramTabItem(
+                  label: '',
+                  page: const Text(''),
+                  icon: InstagramIcons.instagram_reels),
+              InstagramTabItem(
+                  label: '',
+                  page: const Text(''),
+                  icon: InstagramIcons.instagram_user)
+            ],
+            iconType: IconType.icon));
   }
 }
