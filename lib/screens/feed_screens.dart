22f2432be2474f352e7e10228006e5c6abd5f03e
vishnuprasad01 <vishnuprasad.mukundan@gmail.com>
2023-08-19 13:44:55 +0530

    Post Card UI created for feed

--- lib/screens/feed_screens.dart
+++ lib/screens/feed_screens.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram_clone/constants/colors.dart';
 import 'package:instagram_clone/constants/instagram_icons_icons.dart';
+import 'package:instagram_clone/widgets/post_card.dart';
 
 class FeedScreen extends StatefulWidget {
   const FeedScreen({super.key});
@@ -15,36 +16,37 @@ class _FeedScreenState extends State<FeedScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-        toolbarHeight: 70,
-        backgroundColor: mobileBackgroundColor,
-        centerTitle: false,
-        title: Padding(
-          padding: const EdgeInsets.only(top: 28, left: 3),
-          child: SvgPicture.asset(
-            'assets/ic_instagram.svg',
-            color: primaryColor,
-            height: 36,
+        appBar: AppBar(
+          toolbarHeight: 70,
+          backgroundColor: mobileBackgroundColor,
+          centerTitle: false,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 28, left: 3),
+            child: SvgPicture.asset(
+              'assets/ic_instagram.svg',
+              color: primaryColor,
+              height: 36,
+            ),
           ),
+          actions: [
+            // IconButton(
+            //     padding: const EdgeInsets.only(top: 28, right: 4, left: 10),
+            //     onPressed: () {},
+            //     icon: const Icon(InstagramIcons.settings)),
+            IconButton(
+                padding: const EdgeInsets.only(top: 28, right: 15, left: 10),
+                onPressed: () {},
+                icon: const Icon(InstagramIcons.like)),
+            IconButton(
+                padding: const EdgeInsets.only(
+                  top: 28,
+                  right: 8,
+                ),
+                onPressed: () {},
+                icon: const Icon(InstagramIcons.chat)),
+          ],
         ),
-        actions: [
-          IconButton(
-              padding: const EdgeInsets.only(top: 28, right: 4, left: 10),
-              onPressed: () {},
-              icon: const Icon(InstagramIcons.settings)),
-          IconButton(
-              padding: const EdgeInsets.only(top: 28, right: 15, left: 10),
-              onPressed: () {},
-              icon: const Icon(InstagramIcons.like)),
-          IconButton(
-              padding: const EdgeInsets.only(
-                top: 28,
-                right: 8,
-              ),
-              onPressed: () {},
-              icon: const Icon(InstagramIcons.chat))
-        ],
-      ),
+        body: const PostCard(),
       ),
     );
   }
--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,10 +1,163 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constants/colors.dart';
+import 'package:instagram_clone/constants/instagram_icons_icons.dart';
 
 class PostCard extends StatelessWidget {
-  const PostCard({super.key});
+  const PostCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //username and options
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+                .copyWith(right: 0),
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  radius: 16,
+                  backgroundColor: primaryColor,
+                ),
+                const Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [Text('username')],
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => Dialog(
+                        child: ListView(
+                            shrinkWrap: true,
+                            padding: const EdgeInsets.symmetric(vertical: 16),
+                            children: [
+                              'Delete',
+                              'Report',
+                              'Edit',
+                              'Share to',
+                              'Copy Link',
+                              'Cancel',
+                            ]
+                                .map((e) => InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 12, horizontal: 16),
+                                        child: Text(e),
+                                      ),
+                                    ))
+                                .toList()),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.more_horiz_outlined),
+                ),
+              ],
+            ),
+
+            //post content,
+          ),
+          SizedBox(
+              height: MediaQuery.of(context).size.height * 0.30,
+              width: double.infinity,
+              child: Image.network(
+                'https://s3.amazonaws.com/media.mediapost.com/dam/cropped/2023/01/10/image-42_oihgDSL.png',
+                fit: BoxFit.cover,
+              )),
+          //like comment share buttons,
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    InstagramIcons.like,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    InstagramIcons.comment,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    InstagramIcons.arrow_messenger,
+                  )),
+              Expanded(
+                  child: IconButton(
+                      alignment: Alignment.bottomRight,
+                      onPressed: () {},
+                      icon: const Icon(
+                        size: 26,
+                        Icons.bookmark_border_outlined,
+                      )))
+            ],
+          ),
+          //captions and comment numbers,
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+                .copyWith(right: 0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(fontWeight: FontWeight.w900),
+                    child: Text(
+                      '1.3m',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    )),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(top: 8),
+                  child: RichText(
+                    text: const TextSpan(
+                        style: TextStyle(color: primaryColor),
+                        children: [
+                          TextSpan(
+                              text: 'username',
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          TextSpan(
+                              text: '  description or captions came hereee ')
+                        ]),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: const Text(
+                      'View all 10 comments',
+                      style: TextStyle(fontSize: 16, color: secondaryColor),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: const Text(
+                      'Add a comment',
+                      style: TextStyle(fontSize: 14, color: secondaryColor),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
